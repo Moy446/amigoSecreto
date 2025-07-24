9b0f9fb25c13dac089da8268b805c858dc82002f
Moy446 <a22310402@ceti.mx>
2025-07-24 17:32:42 -0600

    desarrollo de logica del juego

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# amigoSecreto
+🎲 Selector Aleatorio de Amigos
 Proyecto del curso de programacion basica en alura
+
+Una página web simple hecha con HTML, CSS y JavaScript puro (Vanilla JS) que permite al usuario:
+
+Escribir nombres de sus amigos
+
+Verlos listados dinámicamente en pantalla
+
+Elegir aleatoriamente a uno de ellos con un solo clic
+
+🧩 Tecnologías utilizadas
+HTML
+
+CSS
+
+JavaScript
+
+🚀 ¿Cómo funciona?
+El usuario ingresa nombres en un campo de texto.
+
+Cada nombre se agrega a una lista visible en la página.
+
+Al hacer clic en el botón "Elegir al azar", se selecciona y muestra uno de los nombres al azar.
+
+📁 Cómo usar
+Clona este repositorio:
+
+'''git clone https://github.com/tu-usuario/nombre-del-repo.git'''
+
+Abre el archivo index.html en tu navegador favorito.
+
+¡Y listo! Comienza a escribir nombres y prueba el selector aleatorio.
--- app.js
+++ app.js
@@ -1,1 +1,47 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(){
+    let amigo  = document.querySelector('#amigo');
+    if (amigo.value == ""){
+        alert("Pon un nombre valido a tu amigo");
+        return 
+    }
+    if(amigos.includes(amigo.value)){
+        amigo.value = ''
+        alert("Este amigo ya esta en tu lista");
+        return 
+    }
+    amigos.push(amigo.value)
+    actualizarLista()
+    amigo.value = ''
+    return 
+}
+
+function actualizarLista(){
+    let lista = document.querySelector('#listaAmigos')
+    lista.innerHTML = ''
+    for (let i = 0; i<amigos.length;i++){
+        let columna = document.createElement('li')
+        columna.textContent = amigos[i]
+        lista.append(columna)
+    }
+}
+
+function posicionSecreto(){
+    return Math.floor(Math.random()* amigos.length)
+}
+
+function sortearAmigo(){
+    if (amigos == ""){
+        alert('No hay amigos en tu lista :c')
+        return
+    }
+    let lista = document.querySelector('#resultado');
+    lista.innerHTML = '';
+    let i = posicionSecreto();
+    lista.append(`El amigo secreto sorteado es: ${amigos[i]}`)
+}
+
+
+
